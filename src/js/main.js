6af8320ff9eef8ace82110dd5c374570d45b40e5
NikLazarenko <nikolay.lazaren@gmail.com>
2016-09-27 18:05:00 +0300

    responsive head

--- src/js/main.js
+++ src/js/main.js
@@ -10,8 +10,50 @@ $(document).ready(function() {
         }
     });
 
-    $('.header__menu a, .about__button').click(function(){
+    $('.header__menu a, .about__button, .header__mobile-link').click(function(){
         $(this).mPageScroll2id({offset: 60});
+    });
+
+    $('.portfolio__items').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    autoplaySpeed: 2000,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true
+                }
+            }
+    ]
+    });
+
+    var clicked = 0;
+    //hamburger handler
+    $('.hamburger').click(function () {
+        $(this).toggleClass('is-active');
+        if (clicked == 0) {
+            $('.header__mobile-menu').removeClass('animated fadeOutRight').addClass('animated fadeInRight header__mobile-menu--is-visible');
+            clicked = 1;
+        } else if (clicked == 1) {
+            $('.header__mobile-menu').removeClass('animated fadeInRight').addClass('animated fadeOutRight');
+            clicked = 0;
+        }
+    });
+
+
+    $(window).resize(function(){
+        if($(this).width() > 768){
+            $('.header__mobile-menu').removeClass('header__mobile-menu--is-visible animated-menu fadeOutRight fadeInRight');
+            $('.hamburger').removeClass('is-active');
+            clicked = 0;
+        }
     });
 
 });
